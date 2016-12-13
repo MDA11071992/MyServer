d147aa5ddb4dfd8939ad874cce7bdacfc2ca24ab
MDA11071992 <d.malashkevich@gmail.com>
2016-12-13 16:13:29 +0300

    commi

---
+++ src/main/java/by/malabar/SampleWebSocketServlet.java
@@ -0,0 +1,31 @@
+package by.malabar;
+
+import org.eclipse.jetty.websocket.WebSocket;
+import org.eclipse.jetty.websocket.WebSocketServlet;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class SampleWebSocketServlet extends WebSocketServlet {
+
+    @Override
+    public WebSocket doWebSocketConnect(HttpServletRequest httpRequest, String protocol) {
+        final String id = httpRequest.getParameter("id");
+        return new WebSocket.OnTextMessage() {
+
+            @Override
+            public void onMessage(String data) {
+                System.out.println(id + " message > " + data);
+            }
+
+            @Override
+            public void onOpen(WebSocket.Connection connection) {
+                System.out.println(id + " open");
+            }
+
+            @Override
+            public void onClose(int closeCode, String message) {
+                System.out.println(id + " close");
+            }
+        };
+    }
+}
